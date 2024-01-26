7310c397ed4622294ee1ff944aa325a7c50a9310
JohBraAdmin <johbra420@gmail.com>
2024-01-26 07:09:57 +0200

    added CORS

--- TaskLoggerApi/TaskLoggerApi/Program.cs
+++ TaskLoggerApi/TaskLoggerApi/Program.cs
@@ -13,6 +13,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors();
 
 var app = builder.Build();
 
@@ -22,6 +23,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:7089"));
 
 app.UseHttpsRedirection();
 
--- TaskLoggerApi/TaskLoggerApi/obj/Debug/net8.0/TaskLoggerApi.AssemblyInfo.cs
+++ TaskLoggerApi/TaskLoggerApi/obj/Debug/net8.0/TaskLoggerApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskLoggerApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+098d36f76bf0470151903442fd641d1b0d275640")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskLoggerApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskLoggerApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
